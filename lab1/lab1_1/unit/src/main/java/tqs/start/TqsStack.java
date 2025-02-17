3a90f63203a4849f1cf489045efc5fbd3cf330d9
neo-rabbit <diogo.m.guedes@ua.pt>
2025-02-17 13:47:48 +0000

    stack and tests

---
+++ lab1/lab1_1/unit/src/main/java/tqs/start/TqsStack.java
@@ -0,0 +1,34 @@
+package tqs.start;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class TqsStack 
+{
+    private ArrayList<Object> stack = new ArrayList<>();
+    public void push(Object obj){
+        stack.add(obj);
+    }
+    public Object pop(){
+        if (stack.isEmpty()){
+            throw new NoSuchElementException();
+        } else {
+        Object obj = stack.remove(0);
+        return obj;
+        }
+    }
+    public Object peek(){
+        if (stack.isEmpty()){
+            throw new NoSuchElementException();
+        } else {
+            Object obj = stack.get(0);
+            return obj;
+        }
+    }
+    public int size(){
+        return stack.size();
+    }
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+}
---
+++ lab1/lab1_1/unit/src/test/java/tqs/start/TqsStackTests.java
@@ -0,0 +1,76 @@
+package tqs.start;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class TqsStackTests 
+{
+    @Test
+    void testStackEmptyOnConstruction(){
+        TqsStack stack = new TqsStack();
+        Assertions.assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    void testStackSizeOnConstruction(){
+        TqsStack stack = new TqsStack();
+        Assertions.assertEquals(0, stack.size());
+    }
+
+    @Test
+    void testStackSizeNotEmpty(){
+        TqsStack stack = new TqsStack();
+        stack.push(1);
+        stack.push("a");
+        stack.push("1");
+        Assertions.assertFalse(stack.isEmpty());
+        Assertions.assertEquals(3, stack.size());
+    }
+
+    @Test
+    void testStackPushPop(){
+        TqsStack stack = new TqsStack();
+        stack.push("z");
+        Object value = stack.pop();
+        Assertions.assertEquals("z", value);
+    }
+
+    @Test
+    void testStackPushPeek(){
+        TqsStack stack = new TqsStack();
+        stack.push("z");
+        Object value = stack.peek();
+        Assertions.assertEquals("z", value);
+        Assertions.assertEquals(1, stack.size());
+    }
+
+    @Test
+    void testStackPushPopMultiple(){
+        TqsStack stack = new TqsStack();
+        stack.push(1);
+        stack.push("a");
+        stack.push("1");
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        Assertions.assertTrue(stack.isEmpty());
+        Assertions.assertEquals(0, stack.size());
+    }
+
+    @Disabled
+    @Test
+    void testStackPopEmptyException(){
+        TqsStack stack = new TqsStack();
+        Assertions.assertThrows(NoSuchElementException.class, () -> stack.pop());
+    }
+
+    @Disabled
+    @Test
+    void testStackPeekEmptyException(){
+        TqsStack stack = new TqsStack();
+        Assertions.assertThrows(NoSuchElementException.class, () -> stack.peek());
+    }
+}
