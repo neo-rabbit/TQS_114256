bf2cff09ba8f5a584efe5241af98268a586cddc7
neo-rabbit <diogo.m.guedes@ua.pt>
2025-02-17 15:37:35 +0000

    1.2 stuff

---
+++ lab1/lab1_2/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tqs.sets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+import tqs.sets.BoundedSetOfNaturals;
+
+/**
+ * @author ico0
+ */
+class BoundedSetOfNaturalsTest {
+    private BoundedSetOfNaturals setA;
+    private BoundedSetOfNaturals setB;
+    private BoundedSetOfNaturals setC;
+
+
+    @BeforeEach
+    public void setUp() {
+        setA = new BoundedSetOfNaturals(3);
+        setB = BoundedSetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+        setC = BoundedSetOfNaturals.fromArray(new int[]{50, 60});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        setA = setB = setC = null;
+    }
+
+    //@Disabled("TODO revise test logic")
+    @Test
+    public void testAddElement() {
+
+        setA.add(99);
+        assertTrue(setA.contains(99), "add: added element not found in set.");
+        assertEquals(1, setA.size());
+
+        assertThrows(IllegalArgumentException.class, () -> setB.add(11));
+    }
+
+    //@Disabled("TODO revise to test the construction from invalid arrays")
+    @Test
+    public void testAddFromBadArray() {
+        int[] elems = new int[]{10, -20, -30};
+
+        // must fail with exception
+        assertThrows(IllegalArgumentException.class, () -> setA.add(elems));
+    }
+
+    @Test
+    public void testAddDuplicateValue(){
+        setA.add(10);
+        assertThrows(IllegalArgumentException.class, () -> setA.add(10));
+    }
+
+    @Test
+    public void testIntersection(){
+        assertFalse(setA.intersects(setB));
+        assertFalse(setA.intersects(setC));
+        assertTrue(setB.intersects(setC));
+    }
+
+}
